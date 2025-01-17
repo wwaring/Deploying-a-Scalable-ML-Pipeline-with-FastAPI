d27482ee8c1d2dc04a85041fac8b788a369dba24
Wendy Waring <wndichu@wgu.edu>
2025-01-17 17:58:40 -0500

    Updated model.py starter code with necessary functions to train, make an inference, and save a model.

--- ml/model.py
+++ ml/model.py
@@ -1,5 +1,6 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
 # TODO: add necessary import
 
@@ -20,7 +21,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +63,8 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +77,15 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    with open(path, "wb") as file:
+        model = pickle.dump(model,file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    with open(path,"rb") as file:
+        model=pickle.load(file)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -118,11 +125,21 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(
         # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
+        # for input data, use data in column given as "column_name", with the slice_va>
         # use training = False
+
+    # Troubleshooting Slicedata NameError by filtering the data for the specified slice
+    sliced_data = data[data[column_name] == slice_value]
+
+    X_slice, y_slice, _, _ = process_data(
+        sliced_data,
+        categorical_features = categorical_features,
+        label = label,
+        training = False,
+        encoder = encoder,
+        lb = lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
